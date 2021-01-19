354e34f22d07f3adfe121bc86a90baf07fce9814
Victor Kushnir <kushnir.victor@gmail.com>
2021-01-19 08:49:44 +0200

    .

--- Part B./main.c
+++ Part B./main.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
-#include <net/ethernet.h>
-#include <netinet/ip.h>
 #include <netinet/if_ether.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <linux/if_packet.h>
-#include <pcap.h>
-#include <stdlib.h>
-
+#include <netinet/ip_icmp.h>
+#include <unistd.h>
 //* RUN THROUGH LINUX TERMINAL AS: sudo ./main .
 
 typedef enum {
@@ -17,13 +12,23 @@ typedef enum {
     FALSE = 0
 } boolean;
 
+typedef struct sockaddr_in sockaddr_in;
+typedef struct packet_mreq packet_mreq;
+typedef struct sockaddr sockaddr;
+typedef struct iphdr iphdr;
+typedef struct icmphdr icmphdr;
+typedef struct ethhdr ethhdr;
+
 #define MTU 1024
 
 int main() {
     int PACKET_LEN = 65536;
     char buffer[PACKET_LEN];
-    struct packet_mreq mr;
-
+    packet_mreq mr;
+    sockaddr saddr;
+    sockaddr_in source, dest;
+    iphdr *ip;
+    icmphdr *icmp;
     // Create the raw socket
     int sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (sock == -1) {
@@ -33,13 +38,27 @@ int main() {
     // Turn on the promiscuous mode.
     mr.mr_type = PACKET_MR_PROMISC;
     setsockopt(sock, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mr, sizeof(mr));
-
+    unsigned int type;
+    unsigned int code;
+    printf("_________________________________________________________________________\n");
+    printf("ICMP sniffing process initiated please wait till it will catch something:\n");
+    printf("_________________________________________________________________________\n");
     // Getting captured packets
     while (TRUE) {
         bzero(buffer, PACKET_LEN);
-        int data_size = recvfrom(sock, buffer, PACKET_LEN, 0, NULL, NULL);
-        if (data_size == -1) {
+        int data_size = recvfrom(sock, buffer, PACKET_LEN, 0, &saddr, (socklen_t*)sizeof(saddr));
+        if (data_size == 0) {
             goto clear;
+        }
+        ip = (iphdr *) (buffer + sizeof(ethhdr));
+        if (ip->protocol == 1) {
+            icmp = (icmphdr *) (buffer + sizeof(ethhdr) + sizeof(iphdr));
+            memset(&source, 0, sizeof(source));
+            source.sin_addr.s_addr = ip->saddr;
+            memset(&dest, 0, sizeof(dest));
+            dest.sin_addr.s_addr = ip->daddr;
+            printf("Source IP:%s\nDesination IP:%s\nType:%d\nCode:%d\n", inet_ntoa(source.sin_addr), inet_ntoa(dest.sin_addr),(unsigned int) icmp->type, (unsigned int) icmp->code);
+            printf("_______________________________________________________________________\n");
         }
     }
     clear:
